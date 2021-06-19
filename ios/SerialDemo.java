96a1212da4905fd81dd73c0cf51294031ba4aa3b
TanmayNankar <tanmaynankar03@gmail.com>
2021-06-19 11:49:47 +0530

    New
    Signed-off-by: TanmayNankar <tanmaynankar03@gmail.com>

---
+++ ios/SerialDemo.java
@@ -0,0 +1,67 @@
+package com.cg.demo.ios;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class SerialDemo {
+
+	public static void main(String[] args)  {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter id: ");
+		int id = sc.nextInt();
+		
+		System.out.println("Enter name: ");
+		String name = sc.next();
+		
+		System.out.println("Enter salary: ");
+		double salary = sc.nextDouble();
+		
+		Employee emp = new Employee(id, name, salary);		
+//		String file = sc.next();		
+		
+		String file = "emp.ser";
+		
+		
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(fos);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		try {
+			oos.writeObject(emp);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		System.out.println(emp.toString());
+		System.out.println("Done");
+
+		
+		try {
+			oos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+}
